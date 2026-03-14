cc238026d8ac787bd519590052e3f47e705a612c
syedismailart <syedismailart@gmail.com>
2026-03-14 05:49:16 +0530

    fix: remove invalid react import in onboarding that broke build

--- frontend/pages/onboarding.tsx
+++ frontend/pages/onboarding.tsx
@@ -1,4 +1,4 @@
-import React, { items, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
 import { useAuthStore } from '../lib/store/authStore';
